44c7b8411496cd3c8f0ebdc8dfb04fa808436c53
nexcontelecom <65533931+nexcontelecom@users.noreply.github.com>
2024-04-08 20:26:16 +0900

    .

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Item from "@enact/sandstone/Item";
 import onairList from "./streaming/onairList.json";
 import cableList from "./streaming/cableList.json";
@@ -21,6 +22,42 @@ const OPEN_URL_LIST = [
   "TV조선2",
 ];
 
+const OnairComponent = React.memo(
+  ({ index, name }) => {
+    const openURL = (a) => {
+      window.open(a, "_blank");
+      return !1;
+    };
+
+    const downloadM3U8 = (url) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          const b = res.channel_item[0].service_url;
+          openURL(b);
+        });
+    };
+
+    const onClickOnair = async (index) => {
+      const { name, url } = onairList[index];
+      if (M3U8_LIST.includes(name)) {
+        downloadM3U8(url);
+      } else if (OPEN_URL_LIST.includes(name)) {
+        openURL(url);
+      }
+    };
+
+    return (
+      <Item onClick={() => onClickOnair(index)} className="onairItem">
+        {name}
+      </Item>
+    );
+  },
+  () => true
+);
+
+const SpottableOnairComponent = Spottable(OnairComponent);
+
 const Main = () => {
   const streamingItemList = useRef([]);
 
@@ -37,7 +74,7 @@ const Main = () => {
     ];
     streamingItemList.current[0].focus();
     Spotlight.focus("onair_1", {
-      enterTo: "default-element",
+      enterTo: "last-focused",
       toOuterContainer: true,
     });
   }, []);
@@ -90,16 +127,11 @@ const Main = () => {
     >
       <div class="column" style={{ flex: 1, backgroundColor: "black" }}>
         {onairList.map((streaming, index) => (
-          <Item
+          <SpottableOnairComponent
             key={`onair_${index}`}
             spotlightId={`onair_${index}`}
-            onSpotlightDown={() => onSpotlightDown(index)}
-            onSpotlightUp={() => onSpotlightUp(index)}
-            onClick={() => onClickOnair(index)}
-            className="onairItem"
-          >
-            {streaming.name}
-          </Item>
+            name={streaming.name}
+          />
         ))}
       </div>
       <div class="column" style={{ flex: 1, backgroundColor: "black" }}>
